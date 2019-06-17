f946bac3718b9097d3c0b190587f50ffdae86324
Gogs <gogs@fake.local>
2019-06-17 15:25:34 +0300

    Chapter 1 finished

--- src/com/edu/chapter01/TaxCalculator.java
+++ src/com/edu/chapter01/TaxCalculator.java
@@ -4,11 +4,33 @@ public class TaxCalculator {
 
 	public double calculate(double taxableIncome) {
 		
-		if (taxableIncome > 500_000.00) {
-			return 50_000 + ((taxableIncome - 500_000) / 5);
+		if (isIncomeIn30PercentTaxRange(taxableIncome)) {
+			return deduct30PercentAbove10Lacs(taxableIncome) +
+					calculate(1_000_000);
 		}
 		
-		return taxableIncome / 10;
+		if (isIncomeIn20PercentTaxRange(taxableIncome)) {
+			return deduct20PercentAbove5Lacs(taxableIncome) +
+					calculate(500_000);
+		}
+		
+		return (taxableIncome * .10);
+	}
+
+	private double deduct20PercentAbove5Lacs(double taxableIncome) {
+		return (taxableIncome - 500_000) * .2;
+	}
+
+	private double deduct30PercentAbove10Lacs(double taxableIncome) {
+		return (taxableIncome - 1_000_000) * .3;
+	}
+
+	private boolean isIncomeIn20PercentTaxRange(double taxableIncome) {
+		return taxableIncome > 500_000;
+	}
+
+	private boolean isIncomeIn30PercentTaxRange(double taxableIncome) {
+		return taxableIncome > 1_000_000;
 	}
 
 }
