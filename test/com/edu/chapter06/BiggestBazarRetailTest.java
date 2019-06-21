654774ea9506588eabc6e94dfb06db8770157cb7
bmihovski <bmihovski@live.com>
2019-06-21 20:23:14 +0300

    Mocks are implemented

--- test/com/edu/chapter06/BiggestBazarRetailTest.java
+++ test/com/edu/chapter06/BiggestBazarRetailTest.java
@@ -1,16 +1,25 @@
 package com.edu.chapter06;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class BiggestBazarRetailTest {
+	@Mock
+	PublicAddressSystem pas;
+	@Mock
+	Inventory inventory;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
 	public void sanityCheck() throws Exception {
-		PublicAddressSystem pas = mock(PublicAddressSystem.class);
-		Inventory inventory = mock(Inventory.class);
 		assertNotNull("Inventory object was null", inventory);
 		assertNotNull("PublicAddressSystem object was null", pas);
 	}
